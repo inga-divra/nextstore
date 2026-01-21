16ddd621579a21a94b07240f7fd3cdebb32757b7
inga-divra <106383378+inga-divra@users.noreply.github.com>
2026-01-21 19:31:14 +0200

    feat(db): fix Prisma seed using Postgres adapter

--- db/seed.ts
+++ db/seed.ts
@@ -1,14 +1,19 @@
+import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
-
 import sampleData from './sample-data';
+import { PrismaPg } from '@prisma/adapter-pg';
 
 async function main() {
-    const prisma = new PrismaClient();
-    await prisma.product.deleteMany();
+    const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
+    const prisma = new PrismaClient({ adapter });
 
+    await prisma.product.deleteMany();
     await prisma.product.createMany({ data: sampleData.products });
 
     console.log('Database seeded successfully');
 }
 
-main();
+main().catch((e) => {
+    console.error(e);
+    process.exit(1);
+});
