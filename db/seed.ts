948577040063c797ea7a0f8b8c3ebdef6d97baa9
inga-divra <106383378+inga-divra@users.noreply.github.com>
2026-01-24 19:29:56 +0200

    Update seed.ts

--- db/seed.ts
+++ db/seed.ts
@@ -1,19 +1,22 @@
-import 'dotenv/config';
 import { PrismaClient } from '@prisma/client';
 import sampleData from './sample-data';
-import { PrismaPg } from '@prisma/adapter-pg';
+
+const prisma = new PrismaClient();
 
 async function main() {
-    const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL! });
-    const prisma = new PrismaClient({ adapter });
-
     await prisma.product.deleteMany();
-    await prisma.product.createMany({ data: sampleData.products });
+    await prisma.product.createMany({
+        data: sampleData.products,
+    });
 
     console.log('Database seeded successfully');
 }
 
-main().catch((e) => {
-    console.error(e);
-    process.exit(1);
-});
+main()
+    .catch((e) => {
+        console.error(e);
+        process.exit(1);
+    })
+    .finally(async () => {
+        await prisma.$disconnect();
+    });
