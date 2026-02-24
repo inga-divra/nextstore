bf2b57e407e7c92319508ab14ef2a0d11117f327
inga-divra <106383378+inga-divra@users.noreply.github.com>
2026-02-24 19:39:10 +0200

    feat(auth): configure NextAuth with credentials provider and Prisma adapter

---
+++ auth.ts
@@ -0,0 +1,57 @@
+import NextAuth from 'next-auth'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { prisma } from '@/db/prisma'
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { compareSync } from 'bcrypt-ts-edge';
+
+export const config = {
+    pages: {
+        signIn: '/sign-in',
+        error: '/sign-in'
+    },
+    session: {
+        strategy: 'jwt',
+        maxAge: 30 * 24 * 60 * 60, //30 days
+    },
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        CredentialsProvider({
+            credentials: {
+                email: {
+                    type: 'email',
+                },
+                password: { type: 'password' },
+            },
+            async authorize(credentials) {
+                if (credentials == null) return null
+
+                // Find user in database
+                const user = await prisma.user.findFirst({
+                    where: {
+                        email: credentials.email as string,
+                    },
+                })
+                // Check if user exists and password is correct
+                if (user && user.password) {
+                    const isMatch = compareSync(
+                        credentials.password as string,
+                        user.password
+                    )
+                    // If password is correct, return user object
+                    if (isMatch) {
+                        return {
+                            id: user.id,
+                            name: user.name,
+                            email: user.email,
+                            role: user.role,
+                        }
+                    }
+                }
+                // If user doesn't exist or password is incorrect, return null
+                return null
+            },
+        }),
+    ],
+}
+
+export const { handlers, auth, signIn, signOut } = NextAuth(config)
