38db16cb22f220d12da871be365b4704f7bef82d
inga-divra <106383378+inga-divra@users.noreply.github.com>
2026-04-12 20:28:03 +0300

    feat(cart): implement cart retrieval and item validation logic

--- lib/actions/cart.actions.ts
+++ lib/actions/cart.actions.ts
@@ -1,10 +1,83 @@
 'use server';
 
 import { CartItem } from '@/types';
+import {
+    cookies
+} from 'next/headers';
+import { convertToPlainObject, formatError } from '../utils';
+import { auth } from '@/auth';
+import { prisma } from '@/db/prisma';
+import { cartItemSchema } from '../validators';
+
 
 export const addItemToCart = async (data: CartItem) => {
-    return {
-        success: true,
-        message: 'Item added to the cart',
-    };
+    try {
+        //Check for cart cookie
+        const sessionCartId = (await cookies()).get('sessionCartId')?.value
+        if (!sessionCartId) throw new Error('Cart session not found')
+
+
+        //Get session and user id
+        const session = await auth()
+        const userId = session?.user?.id ? (session.user.id as string) : undefined
+
+        // Get cart from database
+        const cart = await getMyCart();
+
+        // Parse and validate submitted item data
+        const item = cartItemSchema.parse(data);
+
+        // Find product in database
+        const product = await prisma.product.findFirst({
+            where: { id: item.productId },
+        });
+        if (!product) throw new Error('Product not found');
+
+        //TESTING
+        console.log({
+            'Session Cart ID': sessionCartId,
+            'User ID': userId,
+            'Item Requested': item,
+            'Product Found': product,
+            cart: cart,
+        });
+
+        return {
+            success: true,
+            message: 'Item added to the cart',
+        };
+    } catch (error) {
+        return {
+            success: false,
+            message: formatError(error)
+        }
+    }
 };
+
+export const getMyCart = async () => {
+    //Check for cart cookie
+    const sessionCartId = (await cookies()).get('sessionCartId')?.value
+    if (!sessionCartId) throw new Error('Cart session not found')
+
+
+    //Get session and user id
+    const session = await auth()
+    const userId = session?.user?.id ? (session.user.id as string) : undefined
+
+    //Get user cart from database
+    const cart = await prisma.cart.findFirst({
+        where: userId ? { userId: userId } : { sessionCartId: sessionCartId },
+    })
+
+    if (!cart) return undefined;
+
+    // Convert Decimal values to strings
+    return convertToPlainObject({
+        ...cart,
+        items: cart.items as CartItem[],
+        itemsPrice: cart.itemsPrice.toString(),
+        totalPrice: cart.totalPrice.toString(),
+        shippingPrice: cart.shippingPrice.toString(),
+        taxPrice: cart.taxPrice.toString(),
+    });
+}
