ad29a192041b2efb04912d6fe54241be32dd3c90
inga-divra <106383378+inga-divra@users.noreply.github.com>
2026-04-15 17:19:34 +0300

    feat(cart): add dynamic cart button with quantity controls

--- app/(root)/product/[slug]/page.tsx
+++ app/(root)/product/[slug]/page.tsx
@@ -5,6 +5,7 @@ import { Card, CardContent } from '@/components/ui/card';
 import { Badge } from '@/components/ui/badge';
 import ProductImages from '@/components/shared/product/product-images';
 import AddToCart from '@/components/shared/product/add-to-cart';
+import { getMyCart } from '@/lib/actions/cart.actions';
 
 const ProductDetailsPage = async (props: {
   params: Promise<{ slug: string }>;
@@ -13,6 +14,8 @@ const ProductDetailsPage = async (props: {
 
   const product = await getProductBySlug(slug);
   if (!product) notFound();
+
+  const cart = await getMyCart();
 
   return (
     <>
@@ -67,6 +70,7 @@ const ProductDetailsPage = async (props: {
                 {product.stock > 0 && (
                   <div className=' flex-center'>
                     <AddToCart
+                      cart={cart}
                       item={{
                         productId: product.id,
                         name: product.name,
--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -2,12 +2,18 @@
 import { Button } from '@/components/ui/button';
 import { ToastAction } from '@/components/ui/toast';
 import { useToast } from '@/hooks/use-toast';
-import { addItemToCart } from '@/lib/actions/cart.actions';
-import { CartItem } from '@/types';
-import { Plus } from 'lucide-react';
+import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions';
+import { Cart, CartItem } from '@/types';
+import { Plus, Minus } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-
-const AddToCart = ({ item }: { item: Omit<CartItem, 'cartId'> }) => {
+removeItemFromCart;
+const AddToCart = ({
+  cart,
+  item,
+}: {
+  cart?: Cart;
+  item: Omit<CartItem, 'cartId'>;
+}) => {
   const router = useRouter();
   const { toast } = useToast();
 
@@ -39,9 +45,36 @@ const AddToCart = ({ item }: { item: Omit<CartItem, 'cartId'> }) => {
     });
   };
 
-  return (
+  // Remove item from cart
+  const handleRemoveFromCart = async () => {
+    const res = await removeItemFromCart(item.productId);
+
+    toast({
+      variant: res.success ? 'default' : 'destructive',
+      description: res.message,
+    });
+
+    return;
+  };
+
+  //Check if item is in cart
+  const existItem =
+    cart && cart.items.find((x) => x.productId === item.productId);
+
+  return existItem ? (
+    <div>
+      <Button type='button' variant='outline' onClick={handleRemoveFromCart}>
+        <Minus className='w-4 h-4' />
+      </Button>
+      <span className='px-2'>{existItem.qty}</span>
+      <Button type='button' variant='outline' onClick={handleAddToCart}>
+        <Plus className='w-4 h-4' />
+      </Button>
+    </div>
+  ) : (
     <Button className='w-full' type='button' onClick={handleAddToCart}>
-      <Plus /> Add To Cart
+      <Plus className='w-4 h-4' />
+      Add to cart
     </Button>
   );
 };
