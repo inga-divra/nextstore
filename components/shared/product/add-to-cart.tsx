dca9715eb0d7e36d2122aebdd56f2effab4a5083
inga-divra <106383378+inga-divra@users.noreply.github.com>
2026-04-16 19:45:29 +0300

    feat(cart): add loading state to cart actions with useTransition

--- components/shared/product/add-to-cart.tsx
+++ components/shared/product/add-to-cart.tsx
@@ -4,9 +4,10 @@ import { ToastAction } from '@/components/ui/toast';
 import { useToast } from '@/hooks/use-toast';
 import { addItemToCart, removeItemFromCart } from '@/lib/actions/cart.actions';
 import { Cart, CartItem } from '@/types';
-import { Plus, Minus } from 'lucide-react';
+import { Plus, Minus, Loader } from 'lucide-react';
 import { useRouter } from 'next/navigation';
-removeItemFromCart;
+import { useTransition } from 'react';
+
 const AddToCart = ({
   cart,
   item,
@@ -17,44 +18,50 @@ const AddToCart = ({
   const router = useRouter();
   const { toast } = useToast();
 
+  const [isPending, startTransition] = useTransition();
+
   const handleAddToCart = async () => {
-    // Execute the addItemToCart action
-    const res = await addItemToCart(item);
+    startTransition(async () => {
+      // Execute the addItemToCart action
+      const res = await addItemToCart(item);
 
-    // Display appropriate toast message based on the result
-    if (!res.success) {
+      // Display appropriate toast message based on the result
+      if (!res.success) {
+        toast({
+          variant: 'destructive',
+          description: res.message,
+        });
+        return;
+      }
+
+      //Handle success add to cart
       toast({
-        variant: 'destructive',
         description: res.message,
+        action: (
+          <ToastAction
+            className='bg-primary text-white hover:bg-gray-800'
+            onClick={() => router.push('/cart')}
+            altText='Go to cart'
+          >
+            Go to cart
+          </ToastAction>
+        ),
       });
-      return;
-    }
-
-    //Handle success add to cart
-    toast({
-      description: res.message,
-      action: (
-        <ToastAction
-          className='bg-primary text-white hover:bg-gray-800'
-          onClick={() => router.push('/cart')}
-          altText='Go to cart'
-        >
-          Go to cart
-        </ToastAction>
-      ),
     });
   };
 
   // Remove item from cart
   const handleRemoveFromCart = async () => {
-    const res = await removeItemFromCart(item.productId);
+    startTransition(async () => {
+      const res = await removeItemFromCart(item.productId);
 
-    toast({
-      variant: res.success ? 'default' : 'destructive',
-      description: res.message,
+      toast({
+        variant: res.success ? 'default' : 'destructive',
+        description: res.message,
+      });
+
+      return;
     });
-
-    return;
   };
 
   //Check if item is in cart
@@ -63,17 +70,44 @@ const AddToCart = ({
 
   return existItem ? (
     <div>
-      <Button type='button' variant='outline' onClick={handleRemoveFromCart}>
-        <Minus className='w-4 h-4' />
+      <Button
+        type='button'
+        variant='outline'
+        disabled={isPending}
+        onClick={handleRemoveFromCart}
+      >
+        {isPending ? (
+          <Loader className='w-4 h-4  animate-spin' />
+        ) : (
+          <Minus className='w-4 h-4' />
+        )}
       </Button>
       <span className='px-2'>{existItem.qty}</span>
-      <Button type='button' variant='outline' onClick={handleAddToCart}>
-        <Plus className='w-4 h-4' />
+      <Button
+        type='button'
+        variant='outline'
+        disabled={isPending}
+        onClick={handleAddToCart}
+      >
+        {isPending ? (
+          <Loader className='w-4 h-4 animate-spin' />
+        ) : (
+          <Plus className='w-4 h-4' />
+        )}
       </Button>
     </div>
   ) : (
-    <Button className='w-full' type='button' onClick={handleAddToCart}>
-      <Plus className='w-4 h-4' />
+    <Button
+      className='w-full'
+      type='button'
+      disabled={isPending}
+      onClick={handleAddToCart}
+    >
+      {isPending ? (
+        <Loader className='w-4 h-4 animate-spin' />
+      ) : (
+        <Plus className='w-4 h-4' />
+      )}
       Add to cart
     </Button>
   );
