2e3290fb7b078a12227642b57795743f7c9ccc34
inga-divra <106383378+inga-divra@users.noreply.github.com>
2026-04-22 18:13:57 +0300

    feat(shipping): add shipping address page and getUserById action

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -70,3 +70,13 @@ export async function signUpUser(prevState: unknown, formData: FormData) {
         };
     }
 }
+
+// Get user by ID
+export const getUserById = async (userId: string) => {
+    const user = await prisma.user.findFirst({
+        where: { id: userId },
+    });
+
+    if (!user) throw new Error('User not found');
+    return user;
+}
