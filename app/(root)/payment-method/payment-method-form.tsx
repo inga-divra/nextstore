68c0605913351b1aa59f51b8962c2d46056b8fd2
inga-divra <106383378+inga-divra@users.noreply.github.com>
2026-05-13 18:19:48 +0300

    feat(payment): create payment method page and initialize form state

---
+++ app/(root)/payment-method/payment-method-form.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import CheckoutSteps from '@/components/shared/checkout-steps';
+import { useToast } from '@/hooks/use-toast';
+import { DEFAULT_PAYMENT_METHOD } from '@/lib/constants';
+import { paymentMethodSchema } from '@/lib/validators';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useRouter } from 'next/navigation';
+import { useTransition } from 'react';
+import { useForm } from 'react-hook-form';
+import z from 'zod';
+
+const PaymentMethodForm = ({
+  preferredPaymentMethod,
+}: {
+  preferredPaymentMethod: string | null;
+}) => {
+  const router = useRouter();
+  const { toast } = useToast();
+
+  const form = useForm<z.infer<typeof paymentMethodSchema>>({
+    resolver: zodResolver(paymentMethodSchema),
+    defaultValues: {
+      type: preferredPaymentMethod || DEFAULT_PAYMENT_METHOD,
+    },
+  });
+
+  const [isPending, startTransition] = useTransition();
+  return (
+    <>
+      <CheckoutSteps current={2} />
+    </>
+  );
+};
+
+export default PaymentMethodForm;
