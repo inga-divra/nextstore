7693668411837ebcd344fb07843c819ba51bb4b4
inga-divra <106383378+inga-divra@users.noreply.github.com>
2026-05-21 19:16:56 +0300

    feat(order): add insert order validation schema

--- lib/validators.ts
+++ lib/validators.ts
@@ -87,3 +87,16 @@ export const paymentMethodSchema = z
         path: ['type'],
         message: 'Invalid payment method',
     });
+
+// Insert Order Schema
+export const insertOrderSchema = z.object({
+    userId: z.string().min(1, 'User is required'),
+    itemsPrice: currency,
+    shippingPrice: currency,
+    taxPrice: currency,
+    totalPrice: currency,
+    paymentMethod: z.string().refine((data) => PAYMENT_METHODS.includes(data), {
+        message: 'Invalid payment method',
+    }),
+    shippingAddress: shippingAddressSchema,
+});
