f9b6faf453214bb5815f02bf797fa8509a3df209
inga-divra <106383378+inga-divra@users.noreply.github.com>
2026-05-28 17:14:03 +0300

    feat(place-order): add form submission state and place order action trigger

--- app/(root)/place-order/place-order-form.tsx
+++ app/(root)/place-order/place-order-form.tsx
@@ -4,11 +4,40 @@ import { useFormStatus } from 'react-dom';
 import { Button } from '@/components/ui/button';
 import { createOrder } from '@/lib/actions/order-actions';
 import { useRouter } from 'next/navigation';
+import { log } from 'console';
 
 const PlaceOrderForm = () => {
   const router = useRouter();
 
-  return <>Place order form</>;
+  const handleSubmit = async (event: React.SyntheticEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const res = await createOrder();
+
+    if (res.redirectTo) {
+      router.push(res.redirectTo);
+    }
+  };
+
+  const PlaceOrderButton = () => {
+    const { pending } = useFormStatus();
+    return (
+      <Button disabled={pending} className='w-full'>
+        {pending ? (
+          <Loader className='w-4 h-4 animate-spin' />
+        ) : (
+          <Check className='w-4 h-4' />
+        )}
+        Place Order
+      </Button>
+    );
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className='w-full'>
+      <PlaceOrderButton />
+    </form>
+  );
 };
 
 export default PlaceOrderForm;
