cfe2bb2371dba493524e41cdf410610e4c2bcf62
inga-divra <106383378+inga-divra@users.noreply.github.com>
2026-06-20 18:25:57 +0300

    feat(payment): define payment result Zod schema and TypeScript type

--- lib/validators.ts
+++ lib/validators.ts
@@ -110,3 +110,11 @@ export const insertOrderItemSchema = z.object({
     price: currency,
     qty: z.number(),
 });
+
+//Payment result
+export const paymentResultSchema = z.object({
+    id: z.string(),
+    status: z.string(),
+    email_address: z.string(),
+    pricePaid: z.string(),
+});
